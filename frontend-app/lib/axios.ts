4d736c921fdd6063e413429d61b9adb6242071dd
PRASHANT S BISHT <prashantjee2025@gmail.com>
2025-11-09 00:33:42 +0530

    Update axios.ts

---
+++ frontend-app/lib/axios.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { getToken } from "@/features/auth/tokenStorage";
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  headers: { "Content-Type": "application/json" },
+});
+
+instance.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default instance;
