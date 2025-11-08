eea32b0b8973885e80028d7a02ad3762702c64f1
PRASHANT S BISHT <prashantjee2025@gmail.com>
2025-11-08 20:44:29 +0530

    Add Google Sign-In component with Firebase setup

---
+++ frontend-app/(auth)/login/components/GoogleSignIn.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_FIREBASE_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_FIREBASE_PROJECT_ID",
+  storageBucket: "YOUR_FIREBASE_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
